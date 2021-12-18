c4f1cd2f2c8c3a8681a80bff57dc8a975b2b07a8
Ubuntu <ubuntu@ip-10-0-0-80.ec2.internal>
2021-12-18 09:05:50 +0000

    delete node modules cache, nambah beberapa utils function

---
+++ utils.js
@@ -0,0 +1,12 @@
+const getMethods = (obj) => {
+  let properties = new Set()
+  let currentObj = obj
+  do {
+    Object.getOwnPropertyNames(currentObj).map(item => properties.add(item))
+  } while ((currentObj = Object.getPrototypeOf(currentObj)))
+  return [...properties.keys()].filter(item => typeof obj[item] === 'function')
+}
+
+module.exports = {
+	getMethods,
+}
